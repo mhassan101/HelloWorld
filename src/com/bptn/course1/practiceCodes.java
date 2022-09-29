00cdde88b04fe539914563df0f61a995f67f7392
Mohamud hassan <mohamudhassan@Mohamuds-MacBook-Air.local>
2022-09-29 15:45:15 -0400

    first commit

---
+++ src/com/bptn/course1/practiceCodes.java
@@ -0,0 +1,13 @@
+package com.bptn.course1;
+
+public class practiceCodes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	int[]num: [12,22,3,2,1,33,4];
+	
+	
+
+}
+ 
